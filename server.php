e8d1755e1da4c80caa8ecf06627ee761df94c635
Marcos Vinicius <mv85784246@gmail.com>
2020-12-16 20:59:36 -0300

    Criando um crud simples usando php e mysql em uma única página

---
+++ server.php
@@ -0,0 +1,46 @@
+<?php
+    session_start();
+    $name = "";
+    $address = "";
+    $id = 0;
+    $edit_state = false;
+
+    $db = mysqli_connect('localhost', 'root', '', 'crud');
+
+
+
+    //save records
+    if (isset($_POST['save'])) {
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+
+        $query = "INSERT INTO info (name, address) VALUES ('$name','$address')";
+        mysqli_query($db, $query);
+        $_SESSION['msg'] = "Address saved";
+        header('location: index.php');
+    }
+
+
+    //update records
+    if (isset($_POST['update'])) {
+        $name = mysqli_real_escape_string ($db, $_POST['name']);
+        $address = mysqli_real_escape_string ($db, $_POST['address']);
+        $id = mysqli_real_escape_string ($db, $_POST['id']);
+
+        mysqli_query($db, "UPDATE info SET name='$name', address='$address' WHERE id=$id");
+        $_SESSION['msg'] = "Address updated";
+        header('location: index.php');
+    }
+
+
+    //delete records
+    if (isset($_GET['del'])) {
+        $id = $_GET['del'];
+        
+        mysqli_query($db, "DELETE FROM info WHERE id=$id");
+        $_SESSION['message'] = "Address deleted!"; 
+        header('location: index.php');
+    }
+
+    $results = mysqli_query($db, "SELECT * FROM info");
+?>
